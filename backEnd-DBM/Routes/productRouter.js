7888a90e28ffd9b9962283d30eba48ec9a4fc0d9
Rashid <rasvpz@gmail.com>
2022-09-03 22:57:53 +0530

    products crud frondend and backend finished  jwt user registration backend completed final update

--- backEnd-DBM/Routes/productRouter.js
+++ backEnd-DBM/Routes/productRouter.js
@@ -1,21 +1,22 @@
 const productController = require('../Controller/productController.js')
 const reviewController = require('../Controller/reviewController.js')
+const { checkToken } = require('../auth/token-Validation.js')
 
 // router Product Controller
 const router = require('express').Router()
 // use routers
-router.post('/addProduct', productController.addProduct)
-router.get('/allProducts', productController.getAllProducts)
-router.get('/published', productController.getPublishedProduct)   
-router.get('/products/:id', productController.getOneProduct)
+router.post('/addProduct', checkToken, productController.addProduct)
+router.get('/allProducts', checkToken, productController.getAllProducts)
+router.get('/published', checkToken, productController.getPublishedProduct)   
+router.get('/products/:id', checkToken, productController.getOneProduct)
 // Review Url and Controller
-router.post('/addReview/:id', reviewController.addReview)
-router.get('/allReviews', reviewController.getAllReviews)
-router.get('/getProductReviews/:id', productController.getProductReviews)
+router.post('/addReview/:id', checkToken, reviewController.addReview)
+router.get('/allReviews', checkToken, reviewController.getAllReviews)
+router.get('/getProductReviews/:id', checkToken, productController.getProductReviews)
 
-router.get('/:id', productController.getOneProduct)
-router.put('/:id', productController.updateProduct)
-router.delete('products/:id', productController.deleteProduct)
+router.get('/:id', checkToken, productController.getOneProduct)
+router.put('/:id', checkToken, productController.updateProduct)
+router.delete('products/:id', checkToken, productController.deleteProduct)
 // Review Url and Controller
 
 
